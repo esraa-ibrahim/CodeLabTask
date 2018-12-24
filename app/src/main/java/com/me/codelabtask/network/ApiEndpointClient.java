7ff0f20efbe49bf6b7b693f15e3a752735073947
esraa-ibrahim <esraacs2012@gmail.com>
2018-12-24 19:19:44 +0200

    Second Commit

--- app/src/main/java/com/me/codelabtask/network/ApiEndpointClient.java
+++ app/src/main/java/com/me/codelabtask/network/ApiEndpointClient.java
@@ -1,7 +1,5 @@
 package com.me.codelabtask.network;
 
-import com.me.codelabtask.utils.Constants;
-
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -14,7 +12,7 @@ public class ApiEndpointClient {
     public static ApiEndpointInterface newInstance() {
         if (instance == null) {
             Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.BASE_URL)
+                    .baseUrl("https://reqres.in/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build();
--- app/src/main/java/com/me/codelabtask/utils/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.me.codelabtask.utils;
-
-public class Constants {
-    public static String BASE_URL = "https://reqres.in/";
-}
